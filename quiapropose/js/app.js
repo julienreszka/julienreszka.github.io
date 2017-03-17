c7eda253be551e57d62e7ae1433c4f4646f7f03d
Nicolas Reszka <reszkanicolas@gmail.com>
2017-03-17 20:10:05 +0100

    nouveau projet

---
+++ quiapropose/js/app.js
@@ -0,0 +1,48 @@
+var app = angular.module('app', []);
+
+app.factory('posts', ['$http', function($http) {
+	var url = "data.json"
+    return $http.get(url, {withCredentials: true })
+                .success(function(data) {
+                    return data;
+                    console.log(data);
+                    
+                })
+                .error(function(err) {
+                    return err;
+                    console.log(err);
+                });
+}]);
+
+app.controller('PostController', ['$scope', 'posts', function($scope, posts) {
+	posts.success(function(data) {
+	    $scope.posts = data;
+	    angular.forEach($scope.posts, function(post) {
+  			post.rank = Math.random();
+  		});
+	});
+
+
+
+}]);
+
+app.filter('advanceFilter', ['$filter', function($filter){
+	return function(data, text){
+		if(text != undefined){
+			var textArr = text.split(' ');
+			angular.forEach(textArr, function(test){
+				if(test){
+					data = $filter('filter')(data, test);
+				}
+			})
+			return data;
+		}
+		else{
+			return data;
+		}
+	};
+
+	
+}]);
+
+
