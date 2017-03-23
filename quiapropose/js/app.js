5987e8d6efc3016474c46b0fdf4ed1f54faeb78b
julienreszka <contact.reszkajulien@gmail.com>
2017-03-23 11:00:19 +0100

    modification controller date avant election

--- quiapropose/js/app.js
+++ quiapropose/js/app.js
@@ -3,28 +3,39 @@ var app = angular.module('app', []);
 app.factory('posts', ['$http', function($http) {
 	var url = "data.json"
     return $http.get(url, {withCredentials: true })
-                .success(function(data) {
-                    return data;
-                    console.log(data);
-                    
-                })
-                .error(function(err) {
-                    return err;
-                    console.log(err);
-                });
+        .success(function(data) {
+            return data;
+            console.log(data);
+            
+        })
+        .error(function(err) {
+            return err;
+            console.log(err);
+        });
+}]);
+
+app.controller('DateController', ['$scope', function($scope) {
+
+	var firstRound = new Date(2017, 3, 23);
+	var current = new Date();
+	var datum = ((firstRound - current) / 1000 / 60 / 60 / 24) ;
+
+	$scope.date = datum;
 }]);
 
 app.controller('PostController', ['$scope', 'posts', function($scope, posts) {
 	posts.success(function(data) {
-	    $scope.posts = data;
-	    angular.forEach($scope.posts, function(post) {
-  			post.rank = Math.random();
-  		});
+		$scope.posts = data;
+		angular.forEach($scope.posts, function(post) {
+			post.rank = Math.random();
+		});
 	});
 
 	$scope.firstRound = new Date(2017, 3, 23);
 	$scope.date = new Date();
 }]);
+
+
 
 app.filter('advanceFilter', ['$filter', function($filter){
 	return function(data, text){
